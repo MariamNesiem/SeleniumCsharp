998d015ab3e0243883ccf4f6e030cf4e215dd08a
MariamNesiem <mariam.nesiem@yahoo.com>
2020-10-22 15:38:23 +0200

    create test ReloadHomePageOnBack

--- 04NavigateToURL/CreditCards.UITests/CreditCardWebAppShould.cs
+++ 04NavigateToURL/CreditCards.UITests/CreditCardWebAppShould.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Threading;
 using Xunit;
 
 namespace CreditCards.UITests
@@ -12,10 +13,12 @@ namespace CreditCards.UITests
         {
             driver = new ChromeDriver();
             driver.Navigate().GoToUrl("http://localhost:44108/");
+            Thread.Sleep(3000);
         }
 
         ~CreditCardWebAppShould()
         {
+
            driver.Dispose();
         }
 
@@ -23,7 +26,19 @@ namespace CreditCards.UITests
         [Trait("Category", "Smoke")]
         public void LoadApplicationPage()
         {
-                
+            driver.Navigate().Refresh();
+            Assert.Equal("Home Page - Credit Cards",driver.Title);
+            Assert.Equal("http://localhost:44108/", driver.Url);
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePageOnBack()
+        {
+            driver.Navigate().GoToUrl("http://localhost:44108/Home/About");
+            driver.Navigate().Back();
+            Assert.Equal("Home Page - Credit Cards", driver.Title);
+            Assert.Equal("http://localhost:44108/", driver.Url);
         }
 
     }

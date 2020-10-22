cf62c31ed8f0996fa68ec6816142328c6fb46543
MariamNesiem <mariam.nesiem@yahoo.com>
2020-10-22 14:26:06 +0200

    Create first test

---
+++ 04NavigateToURL/CreditCards.UITests/CreditCardWebAppShould.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+namespace CreditCards.UITests
+{
+    public class CreditCardWebAppShould
+    {
+        public IWebDriver driver;
+        public CreditCardWebAppShould()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:44108/");
+        }
+
+        ~CreditCardWebAppShould()
+        {
+           driver.Dispose();
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+                
+        }
+
+    }
+}
---
+++ 04NavigateToURL/CreditCards2.UITests/CreditCardWebAppShould.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CreditCards2.UITests
+{
+    public class CreditCardWebAppShould
+    {
+        [Fact]
+        [Trait("category","smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver(@"C:\Users\mariam.nesiem\Documents\Studies\SeleniumCsharp\SeleniumCsharp\04NavigateToURL\packages\Selenium.WebDriver.ChromeDriver.86.0.4240.2200\driver\win32"))
+            {
+                driver.Navigate().GoToUrl("http://localhost:44108/");
+            }
+
+        }
+
+    }
+}
